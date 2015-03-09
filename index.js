84808e00bfd80b47e0ab849851ac76f628dfc6ff
s3cy <s3cy@hotmail.com>
2015-03-09 07:36:50 -0400

    move my recent project to github

---
+++ index.js
@@ -0,0 +1,83 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var steinsGate = [
+    {name: "岡部 倫太郎", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/0.png"},
+    {name: "牧瀬 紅莉栖", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/1.png"},
+    {name: "椎名 まゆり", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/2.png"},
+    {name: "橋田 至", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/3.png"},
+    {name: "阿万音 鈴羽", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/4.png"},
+    {name: "フェイリス・ニャンニャン", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/5.png"},
+    {name: "漆原 るか", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/6.png"},
+    {name: "桐生 萌郁", imgURL: "https://raw.githubusercontent.com/s3cy/Steins-Gate-Avatar/master/7.png"}
+];
+
+
+var dollars = [
+    {name: "セットン", imgURL: "https://raw.githubusercontent.com/s3cy/Dollars-Avatar/master/セットン.jpg"},
+    {name: "巴裘拉", imgURL: "https://raw.githubusercontent.com/s3cy/Dollars-Avatar/master/巴裘拉.jpg"},
+    {name: "甘樂", imgURL: "https://raw.githubusercontent.com/s3cy/Dollars-Avatar/master/甘樂.jpg"},
+    {name: "田中太郎", imgURL: "https://raw.githubusercontent.com/s3cy/Dollars-Avatar/master/田中太郎.jpg"},
+    {name: "罪歌", imgURL: "https://raw.githubusercontent.com/s3cy/Dollars-Avatar/master/罪歌.png"}
+];
+
+var data = [
+    {name: "Dollars", roles: dollars, room: 'room0'},
+    {name: "Ei Psy Congloo", roles: steinsGate, room: 'room1'}
+];
+
+var n = 0;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    function connectToRoom(ith) {
+        var room = data[ith].room;
+        var roles = data[ith].roles;
+        socket.join(room);
+        var x = Math.floor(Math.random() * roles.length);
+        socket.nickname = roles[x].name;
+        socket.imgURL = roles[x].imgURL;
+        roles.splice(x, 1);
+        socket.emit('identity', {nick:socket.nickname, imgURL:socket.imgURL, title: data[ith].name});
+        io.in(room).emit('connect message', socket.nickname + " さが入室しました");
+        socket.on('chat message', function(data) {
+            socket.in(room).broadcast.emit('chat message', {msg:data, nick:socket.nickname, imgURL: socket.imgURL});
+        });
+        socket.on('disconnect', function() {
+            io.in(room).emit('connect message', socket.nickname + " さが退室しました");
+            roles.push({name: socket.nickname, imgURL: socket.imgURL});
+        });
+    }
+
+    while (1) {
+        if (data[n].roles.length != 0) {
+            connectToRoom(n);
+            break;
+        } else {
+            for (var i = 0; i < data.length; i++) {
+                var clients = io.sockets.adapter.rooms[data[i].room];
+                var j = 0;
+                for (j in clients) {
+                    j++;
+                }
+                if (j == 0) {
+                    n = i;
+                    break;
+                }
+            }
+            if (data[n].roles.length == 0) {
+                socket.emit('sorry message');
+                socket.disconnect();
+                break;
+            }
+        }
+    }
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
