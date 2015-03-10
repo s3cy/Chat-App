d39aaaae476bbddca28b4961d73e4fe5448c18fe
s3cy <s3cy@hotmail.com>
2015-03-09 20:19:35 -0400

    server console log helper
    help find the status of chat rooms and roles

--- index.js
+++ index.js
@@ -2,54 +2,9 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var data = require("./data.json");
 
-var steinsGate = [
-    {name: "岡部 倫太郎", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/0.png"},
-    {name: "牧瀬 紅莉栖", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/1.png"},
-    {name: "椎名 まゆり", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/2.png"},
-    {name: "橋田 至", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/3.png"},
-    {name: "阿万音 鈴羽", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/4.png"},
-    {name: "フェイリス・ニャンニャン", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/5.png"},
-    {name: "漆原 るか", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/6.png"},
-    {name: "桐生 萌郁", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Steins-Gate-Avatar/7.png"}
-];
-
-
-var dollars = [
-    {name: "セットン", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Dollars-Avatar/セットン.jpg"},
-    {name: "巴裘拉", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Dollars-Avatar/巴裘拉.jpg"},
-    {name: "甘樂", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Dollars-Avatar/甘樂.jpg"},
-    {name: "田中太郎", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Dollars-Avatar/田中太郎.jpg"},
-    {name: "罪歌", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Dollars-Avatar/罪歌.png"}
-];
-
-var fateZero = [
-    {name: "archer", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/archer.jpg"},
-    {name: "assassin", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/assassin.jpg"},
-    {name: "berserker", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/berserker.jpg"},
-    {name: "caster", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/caster.jpg"},
-    {name: "lancer", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/lancer.jpg"},
-    {name: "rider", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/rider.jpg"},
-    {name: "saber", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Fate-Zero-Avatar/saber.jpg"}
-];
-
-var anohana = [
-    {name: "久川 鉄道", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Anohana-Avatar/久川%20鉄道.png"},
-    {name: "安城 鳴子", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Anohana-Avatar/安城%20鳴子.png"},
-    {name: "宿海 仁太", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Anohana-Avatar/宿海%20仁太.png"},
-    {name: "本間 芽衣子", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Anohana-Avatar/本間%20芽衣子.png"},
-    {name: "松雪 集", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Anohana-Avatar/松雪%20集.png"},
-    {name: "鶴見 知利子", imgURL: "https://raw.githubusercontent.com/s3cy/Pictures/master/Anohana-Avatar/鶴見%20知利子.png"}
-];
-
-var data = [
-    {name: "Dollars", roles: dollars, room: 'room0'},
-    {name: "Ei Psy Congloo", roles: steinsGate, room: 'room1'},
-    {name: "Fate Zero", roles: fateZero, room: 'room2'},
-    {name: "超平和バスターズ", roles: anohana, room: 'room3'}
-];
-
-var n = 3;
+var n = Math.floor(Math.random() * data.length);
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
@@ -65,6 +20,7 @@ io.on('connection', function(socket) {
         socket.nickname = roles[x].name;
         socket.imgURL = roles[x].imgURL;
         roles.splice(x, 1);
+        console.log(socket.nickname + " was created!");
         socket.emit('identity', {nick:socket.nickname, imgURL:socket.imgURL, title: data[ith].name});
         io.in(room).emit('connect message', socket.nickname + " さが入室しました");
         socket.on('chat message', function(data) {
@@ -72,6 +28,7 @@ io.on('connection', function(socket) {
         });
         socket.on('disconnect', function() {
             io.in(room).emit('connect message', socket.nickname + " さが退室しました");
+            console.log(socket.nickname + " leaves the room " + data[ith].name);
             roles.push({name: socket.nickname, imgURL: socket.imgURL});
         });
     }
@@ -81,6 +38,7 @@ io.on('connection', function(socket) {
             connectToRoom(n);
             break;
         } else {
+            console.log("room " + data[n].name + " is full")
             for (var i = 0; i < data.length; i++) {
                 var clients = io.sockets.adapter.rooms[data[i].room];
                 var j = 0;
@@ -89,6 +47,7 @@ io.on('connection', function(socket) {
                 }
                 if (j == 0) {
                     n = i;
+                    console.log("room== " + data[n].name " ==room");
                     break;
                 }
             }
@@ -103,4 +62,5 @@ io.on('connection', function(socket) {
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
+    console.log("room== " + data[n].name " ==room");
 });
